a12b551ec8d86068d6a5fd8a79b4a193b61a2668
Manuel Guilbault <manuel.guilbault@gmail.com>
2017-11-27 10:33:14 +0100

    Add exporting SAS token to VSTS variable

--- azure/tools/generate-sas-token.ps1
+++ azure/tools/generate-sas-token.ps1
@@ -1,10 +1,12 @@
 Param(
   [string] $ResourceGroup,
   [string] $StorageAccount,
-  [string] $StorageContainer
+  [string] $StorageContainer,
+  [string] $Permission,
+  [string] $ExportTo
 )
 
 $key = Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroup -Name $StorageAccount
 $context = New-AzureStorageContext -StorageAccountName $StorageAccount -StorageAccountKey $key[0].Value
-$sasToken = New-AzureStorageContainerSASToken -Name $StorageContainer -Permission r -Context $context -ExpiryTime (Get-Date).AddYears(100)
-return $sasToken
+$sasToken = New-AzureStorageContainerSASToken -Name $StorageContainer -Permission $Permission -Context $context -ExpiryTime (Get-Date).AddYears(100)
+Write-Host "##vso[task.setvariable variable=$ExportTo]$sasToken"
