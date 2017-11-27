d5f313a1dddbb225647019a4c1505ec17d5c8a7b
Manuel Guilbault <manuel.guilbault@gmail.com>
2017-11-27 10:17:22 +0100

    Stick to PS parameters naming convention

--- azure/tools/generate-sas-token.ps1
+++ azure/tools/generate-sas-token.ps1
@@ -1,10 +1,10 @@
 Param(
-  [string] $resourceGroup,
-  [string] $storageAccount,
-  [string] $storageContainer
+  [string] $ResourceGroup,
+  [string] $StorageAccount,
+  [string] $StorageContainer
 )
 
-$key = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccount
-$context = New-AzureStorageContext -StorageAccountName $storageAccount -StorageAccountKey $key[0].Value
-$sasToken = New-AzureStorageContainerSASToken -Name $storageContainer -Permission r -Context $context -ExpiryTime (Get-Date).AddYears(100)
+$key = Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroup -Name $StorageAccount
+$context = New-AzureStorageContext -StorageAccountName $StorageAccount -StorageAccountKey $key[0].Value
+$sasToken = New-AzureStorageContainerSASToken -Name $StorageContainer -Permission r -Context $context -ExpiryTime (Get-Date).AddYears(100)
 return $sasToken
